a33cc09da566b01ad7a7043116ee55f4b23eba6a
luoyang <irsluoo@outlook.com>
2021-06-01 09:47:51 +0800

    删除print

--- ppocr/data/imaug/rec_img_aug.py
+++ ppocr/data/imaug/rec_img_aug.py
@@ -402,7 +402,6 @@ def warp(data, ang, use_tia=True, prob=0.4):
     """
     img = data["image"]
     label = data["label"]
-    print("_____ : ", label)
     h, w, _ = img.shape
     config = Config(use_tia=use_tia)
     config.make(w, h, ang)
